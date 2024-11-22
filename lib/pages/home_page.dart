6ad091cd8f3c01c1507addc7dfd70611b9d9f861
zyedshamsuddin <zyedshamsuddin@gmail.com>
2024-11-22 04:47:36 +0000

    add: padding to HomeScreen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,17 +17,20 @@ class HomePage extends StatelessWidget {
         ),
         elevation: 0,
       ),
-      body: ListView.builder(
-        itemCount: 8,
-        itemBuilder: (context, index) {
-          return Tile(
-              title:
-                  'Item $index Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tincidunt risus sit amet odio rhoncus, et imperdiet lorem ultrices. Donec.',
-              subtitle:
-                  'Subtitle $index Nam ut congue mauris. Cras sed neque nunc. Phasellus interdum metus neque, quis tincidunt tortor placerat id. Fusce non diam.',
-              summary:
-                  'Summary $index Integer venenatis, ipsum a bibendum molestie, dolor nisl ullamcorper urna, ut tempor sem orci nec nunc. Etiam leo mauris, volutpat.');
-        },
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: ListView.builder(
+          itemCount: 8,
+          itemBuilder: (context, index) {
+            return Tile(
+                title:
+                    'Item $index Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tincidunt risus sit amet odio rhoncus, et imperdiet lorem ultrices. Donec.',
+                subtitle:
+                    'Subtitle $index Nam ut congue mauris. Cras sed neque nunc. Phasellus interdum metus neque, quis tincidunt tortor placerat id. Fusce non diam.',
+                summary:
+                    'Summary $index Integer venenatis, ipsum a bibendum molestie, dolor nisl ullamcorper urna, ut tempor sem orci nec nunc. Etiam leo mauris, volutpat.');
+          },
+        ),
       ),
     );
   }
