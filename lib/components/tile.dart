a52256654b63cbbde47d3196cb4babfce857f272
zyedshamsuddin <zyedshamsuddin@gmail.com>
2024-11-22 04:48:06 +0000

    changes to Tile UI

--- lib/components/tile.dart
+++ lib/components/tile.dart
@@ -16,86 +16,105 @@ class Tile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Column(
         children: [
-          // Circular dot
-          Container(
-            margin: const EdgeInsets.only(top: 5),
-            width: 10,
-            height: 10,
-            decoration: const BoxDecoration(
-              color: Colors.blue,
-              shape: BoxShape.circle,
-            ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Circular dot
+              Container(
+                margin: const EdgeInsets.only(top: 5),
+                width: 10,
+                height: 10,
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              const SizedBox(width: 10),
+              // Content column
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // First row with Title and date
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            '$title flutter.dev',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Text(
+                          '21.11.2024',
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                        const Icon(
+                          Icons.chevron_right_rounded,
+                          color: Colors.grey,
+                          size: 20,
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 4),
+                    // Second row with Link and flag
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            subtitle,
+                            style: const TextStyle(
+                              fontSize: 14,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Icon(
+                          Icons.flag,
+                          color: Colors.grey[500],
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 4),
+                    // Summary
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            summary,
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey[600],
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        const SizedBox(width: 20)
+                      ],
+                    ),
+                    const SizedBox(height: 4),
+                  ],
+                ),
+              ),
+            ],
           ),
-          const SizedBox(width: 10),
-          // Content column
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // First row with sender and date
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        '$title flutter.dev',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    const SizedBox(width: 8),
-                    Text(
-                      '21.11.2024',
-                      style: TextStyle(
-                        color: Colors.grey[600],
-                      ),
-                    ),
-                    const Icon(
-                      Icons.chevron_right_rounded,
-                      color: Colors.grey,
-                      size: 20,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 4),
-                // Second row with title and flag
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        subtitle,
-                        style: const TextStyle(
-                          fontSize: 14,
-                        ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    const SizedBox(width: 4),
-                    const Icon(
-                      Icons.flag,
-                      color: Colors.black,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 4),
-                // Summary
-                Text(
-                  summary,
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.grey[600],
-                  ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                const SizedBox(width: 16),
-              ],
-            ),
+          Divider(
+            color: Colors.grey[300],
+            indent: 16,
+            endIndent: 16,
+            thickness: 2,
           ),
         ],
       ),
