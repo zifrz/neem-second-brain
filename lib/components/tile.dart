34f35eb9d1925af3d4f2d00b57d4e2541feba2a8
zyedshamsuddin <zyedshamsuddin@gmail.com>
2024-11-24 16:08:17 +0000

    feat: added slidable for tile

--- lib/components/tile.dart
+++ lib/components/tile.dart
@@ -1,125 +1,153 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:neem/models/artifact.dart';
 
-class Tile extends StatelessWidget {
+class Tile extends StatefulWidget {
   final Artifact artifact;
 
-  Tile({
+  const Tile({
     super.key,
     required this.artifact,
   });
 
-  late String title = artifact.title;
-  late String summary = artifact.excerpt ?? "";
-  late String subtitle = artifact.siteName ?? "";
-  late DateTime createdAt = artifact.createdAt;
+  @override
+  State<Tile> createState() => _TileState();
+}
+
+class _TileState extends State<Tile> {
+  late String title = widget.artifact.title;
+  late String summary = widget.artifact.excerpt ?? "";
+  late String subtitle = widget.artifact.siteName ?? "";
+  late bool isRead = widget.artifact.isRead;
+  late DateTime createdAt = widget.artifact.createdAt;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    const padding = EdgeInsets.all(8.0);
+    const spacer = SizedBox(width: 8);
+    const smallSpacer = SizedBox(height: 4);
+
+    return Column(
+      children: [
+        Slidable(
+          startActionPane: ActionPane(
+            motion: const BehindMotion(),
             children: [
-              // Circular dot
-              Container(
-                margin: const EdgeInsets.only(top: 5),
-                width: 10,
-                height: 10,
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
-                  shape: BoxShape.circle,
+              SlidableAction(
+                onPressed: (_) => setState(() => isRead = !isRead),
+                icon: isRead ? Icons.mail_outline_rounded : Icons.mail_rounded,
+                label: isRead ? "Unread" : "Read",
+                backgroundColor: Colors.blue,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(8),
+                  bottomLeft: Radius.circular(8),
                 ),
-              ),
-              const SizedBox(width: 10),
-              // Content column
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // First row with Title and date
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Text(
-                            title,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        const SizedBox(width: 8),
-                        Text(
-                          '${createdAt.day}.${createdAt.month}.${createdAt.year}',
-                          style: TextStyle(
-                            color: Colors.grey[600],
-                          ),
-                        ),
-                        const Icon(
-                          Icons.chevron_right_rounded,
-                          color: Colors.grey,
-                          size: 20,
-                        ),
-                      ],
-                    ),
-
-                    const SizedBox(height: 4),
-                    // Second row with Link and flag
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Text(
-                            subtitle,
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        const SizedBox(width: 8),
-                        Icon(
-                          Icons.flag,
-                          color: Colors.grey[500],
-                        ),
-                      ],
-                    ),
-
-                    const SizedBox(height: 4),
-                    // Summary
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Text(
-                            summary,
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.grey[600],
-                            ),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        const SizedBox(width: 20)
-                      ],
-                    ),
-                    const SizedBox(height: 4),
-                  ],
-                ),
-              ),
+              )
             ],
           ),
-          Divider(
-            color: Colors.grey[300],
-            indent: 16,
-            endIndent: 16,
-            thickness: 2,
+          endActionPane: ActionPane(
+            motion: const StretchMotion(),
+            children: [
+              SlidableAction(
+                onPressed: (_) {},
+                icon: Icons.delete,
+                label: "Delete",
+                backgroundColor: Colors.red,
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(8),
+                  bottomRight: Radius.circular(8),
+                ),
+              )
+            ],
           ),
-        ],
-      ),
+          child: Padding(
+            padding: padding,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 5),
+                  width: 10,
+                  height: 10,
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              title,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          spacer,
+                          Text(
+                            '${createdAt.day}.${createdAt.month}.${createdAt.year}',
+                            style: TextStyle(color: Colors.grey[600]),
+                          ),
+                          const Icon(
+                            Icons.chevron_right_rounded,
+                            color: Colors.grey,
+                            size: 20,
+                          ),
+                        ],
+                      ),
+                      smallSpacer,
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              subtitle,
+                              style: const TextStyle(fontSize: 14),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          spacer,
+                          Icon(Icons.flag, color: Colors.grey[500]),
+                        ],
+                      ),
+                      smallSpacer,
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              summary,
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey[600],
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          const SizedBox(width: 20),
+                        ],
+                      ),
+                      smallSpacer,
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Divider(
+          color: Colors.grey[300],
+          indent: 16,
+          endIndent: 16,
+          thickness: 2,
+        ),
+      ],
     );
   }
 }
